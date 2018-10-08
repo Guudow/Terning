29c51db1544907f4e755c6cca79d5cf1e49a0b2d
Joe-Briefcase <s185143@student.dtu.dk>
2018-10-08 09:37:55 +0200

    testDice med tællere

--- src/terning/DiceTest.java
+++ src/terning/DiceTest.java
@@ -22,6 +22,36 @@ public class DiceTest {
     public void rollMultiple() {
         Dice testDice = new Dice();
         int antalRolls = 60000;
+        int en = 0, to = 0, tre = 0, fire = 0, fem = 0, seks = 0;
+
+        for (int i = 0; i < antalRolls; i++)
+        {
+            int resultat = testDice.roll();
+
+            switch (resultat)
+            {
+                case 1:
+                    en++;
+                    break;
+                case 2:
+                    to++;
+                    break;
+                case 3:
+                    tre++;
+                    break;
+                case 4:
+                    fire++;
+                    break;
+                case 5:
+                    fem++;
+                    break;
+                case 6:
+                    seks++;
+                    break;
+            }
+        }
+
+        System.out.println(en + "\n" + to + "\n" + tre + "\n" + fire + "\n" + fem + "\n" + seks);
 
     }
 }
